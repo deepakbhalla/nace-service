5d3992e0c6cfa70af5d899efccaf49a22b26705c
deepakbhalla <deepak.dbhalla@gmail.com>
2022-01-31 03:23:37 +0530

    swagger config refactoring

--- src/main/java/com/nace/config/SwaggerConfig.java
+++ src/main/java/com/nace/config/SwaggerConfig.java
@@ -2,11 +2,9 @@ package com.nace.config;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -26,16 +24,21 @@ public class SwaggerConfig {
 
     @Bean
     public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.nace.controllers")).paths(regex("/nace.*")).build()
-                .apiInfo(metaData()).useDefaultResponseMessages(false);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.nace.controllers"))
+                .paths(regex("/nace.*")).build()
+                .apiInfo(metaData())
+                .useDefaultResponseMessages(false);
     }
 
     private ApiInfo metaData() {
-        ApiInfo apiInfo = new ApiInfo("NACE DETAILS SERVICE", "Available features:\n 1. Import of NACE details from CSV file to application database\n 2. Fetch NACE records for a given order", "1.0",
-                "",
-                new Contact("Deepak Bhalla", "https://github.com/deepakbhalla", "deepak.dbhalla@gmail.com"), "",
-                "", Collections.emptyList());
-        return apiInfo;
+        return new ApiInfoBuilder().title("NACE DETAILS SERVICE")
+                .description("Available features:\n 1. Import of NACE details from CSV file to application database\n 2. Fetch NACE records for a given order")
+                .contact(new Contact("Deepak Bhalla", "https://github.com/deepakbhalla", "deepak.dbhalla@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("0.0.1-SNAPSHOT")
+                .build();
     }
 }
